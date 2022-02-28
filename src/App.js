74eff3188f61b41ac8a028b9731312524c97b5c0
khushaliargade <95602831+khushaliargade@users.noreply.github.com>
2022-02-28 17:41:00 +0530

    component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Item from './Item'
+const Home = () => {
+    return (
+        <div className="class">
+            <div className="card bg-dark text-white">
+                <img src="/assest/car.jpg" class="card-img" alt="Backgroung"  />
+                
+                 <Item/>
+            </div>
+        </div>
+    )
+}
+
+export default Home
