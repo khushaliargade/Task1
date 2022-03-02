aef52bdbc11390c9348723ac3c49ff207a493fe6
khushaliargade <95602831+khushaliargade@users.noreply.github.com>
2022-03-02 19:15:21 +0530

    wikipedia

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-
-//import Navbar from './component/Navbar';
-import Home from './component/Home';
-import Products from './component/Products';
-import Title from './component/Title';
-
-import {
-  Card, CardBody, CardImg, CardTitle, Col, CardText, CardSubtitle, CardGroup, Button, Row
-} from "reactstrap"
+import React from 'react'
+import Home from './component/Home'
 
 function App() {
-  return (
-    <>
-
-     
-
-      <Row>
-        <Col md="5">
-          <Home /> </Col>
-      
-        <Col md="5">
-          <Title />
-        </Col>
-      </Row>
-
-
-
-    </>
-  );
+    return (
+        <Home />
+    );
 }
 
 export default App;
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,39 +1,62 @@
-import React from 'react'
-import Item from './Item'
-//import Products from './Products'
-//import SampleOutput from "./SampleOutput.json"
+import { useState } from 'react';
+function Home() {
+    const [search, setSearch] = useState("");
+    const [results, setResults] = useState([]);
+    const [searchInfo, setSearchInfo] = useState({});
 
-import {
-    Input, Label
-} from "reactstrap"
+    const handleSearch = async e => {
+        e.preventDefault();
+        if (search === '') return;     // if we dont search anything inside input throw an error
 
+        const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+        const response = await fetch(endpoint);
+        console.log(response);
 
-const Home = () => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+        setResults(json.query.search);
+        setSearchInfo(json.query.searchinfo);
+    }
+
     return (
-        <div className="product">
-            <div className="posts">
-                <img
-                    alt="Card image cap"
-                    src="./assest/plant.jpg"
-                    width="100%"
-                />
+        <div className="App">
+            <header>
+                <h1>wiki seeker</h1>
+                <form className="search-box " onSubmit={handleSearch}>
+                    <input type="search" placeholder="what are you looking for?"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
 
-
-                <div className="reviews">
-                    <Label  tag="h2" >
-                        Reviews
-                    </Label>
-                    <Input
-                        id="exampleText"
-                        name="text"
-                        type="textarea"
                     />
-                </div>
+                </form>
+                {(searchInfo.totalhits) ? <p>search results: {searchInfo.totalhits}</p> : ''}
+            </header>
+            <div className="results">
+                {results.map((result, i) => {
+                    const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
 
+
+                    return (
+                        <div className="result" key={i}>
+                            <h3>{result.title}</h3>
+                            <p dangerouslySetInnerHTML={{
+                                __html: result.snippet
+                            }} ></p>
+                            <a href={url} target="_blank" rel="nofollow">Read More</a>
+
+
+                        </div>
+                    )
+                })}
 
             </div>
         </div>
-    )
+
+    );
 }
 
-export default Home
+export default Home;
+
